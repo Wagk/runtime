335d5e1b7c1687c74d7a1437542052340fd02f05
pang.t <polkafreaku@gmail.com>
2016-09-15 22:10:37 +0800

    Initializing queues within `kernel::init()`
    Next step is to implement the worker

--- Runtime/Runtime/concurrent/kernel.cpp
+++ Runtime/Runtime/concurrent/kernel.cpp
@@ -1,6 +1,67 @@
 #include "kernel.h"
+#include <thread>
+#include <utility>
 
 namespace concurrent
 {
+
+	void kernel::init()
+	{
+		_numthreads = std::thread::hardware_concurrency() - 1;
+
+		_threadpool.resize(_numthreads);
+
+		//main thread
+		_maininfo._kill = false;
+		_maininfo._graphics = true;
+		
+		for (auto& thread : _threadpool)
+		{
+			deque* queueptr = &thread.first._queue;
+			_maininfo._steal_queues.push_back(queueptr);
+		}
+
+		//spawned threads
+		for (auto& thread : _threadpool)
+		{
+			workerinfo& info = thread.first;
+
+			info._steal_queues.reserve(_numthreads + 1);
+			info._steal_queues.push_back(&_maininfo._queue);
+			for (auto& elem : _threadpool)
+			{
+				if(&elem.first._queue != &thread.first._queue)
+					info._steal_queues.push_back(&elem.first._queue);
+			}
+
+			info._kill = false;
+			info._graphics = false;
+		}
+
+		for (auto& thread : _threadpool)
+		{
+			thread.second = std::move(std::thread(&thread.first));
+		}
+
+	}
+
+	void kernel::shutdown()
+	{
+		for (auto& thread : _threadpool)
+		{
+			thread.first._kill = true;
+		}
+
+		for (auto& thread : _threadpool)
+		{
+			thread.second.join();
+		}
+	}
+
+	void kernel::worker(workerinfo* info)
+	{
+
+		
+	}
 
 } //namespace concurrent
--- Runtime/Runtime/concurrent/kernel.h
+++ Runtime/Runtime/concurrent/kernel.h
@@ -8,6 +8,7 @@
 
 #include <thread>
 #include <vector>
+#include "deque.h"
 
 namespace concurrent
 {
@@ -20,8 +21,21 @@ namespace concurrent
 		void shutdown();
 
 	private:
+		
+		struct workerinfo
+		{
+			deque _queue; //thread local queue
+			std::vector<deque*> _steal_queues;//pointers to other queues
+			bool _kill;		//kill flag
+			bool _graphics;	//affinities
+		};
 
-		void worker();
+		void worker(workerinfo* info);
+
+		unsigned int _numthreads;
+
+		workerinfo _maininfo;
+		std::vector<std::pair<workerinfo, std::thread>> _threadpool;
 
 	};
 
